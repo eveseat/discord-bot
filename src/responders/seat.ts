27cf320d20b07f9ce5fcc65f530afa7b142b95b3
VeritasLuxMea <veteranmina@users.noreply.github.com>
2022-01-09 14:29:57 -0600

    Correct handling of axios promise

--- src/responders/seat.ts
+++ src/responders/seat.ts
@@ -1,29 +1,46 @@
 import { Message, MessageEmbed } from 'discord.js'
 import * as log from '../lib/console'
+import axios from 'axios'
 
-const seatdocker = `https://img.shields.io/docker/v/eveseat/seat`
-const api = `https://img.shields.io/github/v/release/eveseat/api`
-const console = `https://img.shields.io/github/v/release/eveseat/console`
-const eveapi = `https://img.shields.io/github/v/release/eveseat/eveapi`
-const notifications = `https://img.shields.io/github/v/release/eveseat/notifications`
-const services = `https://img.shields.io/github/v/release/eveseat/services`
-const web = `https://img.shields.io/github/v/release/eveseat/web`
+const seatdocker = getVersion(`https://img.shields.io/docker/v/eveseat/seat.json`)
+const api = getVersion(`https://img.shields.io/github/v/release/eveseat/api.json`)
+const seat_console = getVersion(`https://img.shields.io/github/v/release/eveseat/console.json`)
+const eveapi = getVersion(`https://img.shields.io/github/v/release/eveseat/eveapi.json`)
+const notifications = getVersion(`https://img.shields.io/github/v/release/eveseat/notifications.json`)
+const services = getVersion(`https://img.shields.io/github/v/release/eveseat/services.json`)
+const web = getVersion(`https://img.shields.io/github/v/release/eveseat/web.json`)
 
 export const matcher = `.seat`
 export const handle = (m: Message): void => {
-    log.debug(`message from ${m.author.username}. sending a seat related response`);
-
-    const mEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Current SeAT Package Versions')
-        .addFields(
-            { name: 'Seat Docker Image', value: `${seatdocker}`},
-            { name: 'API', value: `${api}`},
-            { name: 'Console', value: `Depreciated: ${console}`},
-            { name: 'EveAPI', value: `${eveapi}`},
-            { name: 'Notifications', value: `${notifications}`},
-            { name: 'Services', value: `${services}`},
-            { name: 'Web', value: `${web}`}
-            );
-    m.channel.send( { content: `${m.author}`, embeds: [mEmbed] });
+	log.debug(`message from ${m.author.username}. sending a seat related response`);
+	Promise.all([seatdocker, api, seat_console, eveapi, notifications, services, web])
+		.then(function(values)
+		{
+			const mEmbed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('Current SeAT Package Versions')
+			.addFields(
+				{ name: 'Seat Docker Image', value: `${values[0]}`},     //seatdocker
+				{ name: 'API', value: `${values[1]}`},                   //api
+				{ name: 'Console-Depreciated', value: `${values[2]}`},   //seat_console
+				{ name: 'EveAPI', value: `${values[3]}`},                //eveapi
+				{ name: 'Notifications', value: `${values[4]}`},         //notifications
+				{ name: 'Services', value: `${values[5]}`},              //services
+				{ name: 'Web', value: `${values[6]}`}                    //web
+        );
+		m.channel.send( { content: `${m.author}`, embeds: [mEmbed] });
+		})
+        .catch(error => {
+            m.channel.send(`${m.author}, unable to determine current versions at this time`);
+        })
 };
+
+function getVersion(version: string) {
+return axios.get(version)
+  .then(r => {
+    return (r.data.value)
+  })
+  .catch(error => {
+    log.debug(`Encountered error in !versions response`)
+  })
+}
