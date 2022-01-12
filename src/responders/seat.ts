3170ac57cc80747370d6b623485413d1f5b2d1a9
VeritasLuxMea <veteranmina@users.noreply.github.com>
2022-01-12 09:39:03 -0600

    Update to use Github, correct styling....
    Also add release date to response

--- src/responders/seat.ts
+++ src/responders/seat.ts
@@ -2,15 +2,15 @@ import { Message, MessageEmbed } from 'discord.js'
 import * as log from '../lib/console'
 import axios from 'axios'
 
-const seatdocker = getVersion(`https://img.shields.io/docker/v/eveseat/seat.json`)
-const api = getVersion(`https://img.shields.io/github/v/release/eveseat/api.json`)
-const seat_console = getVersion(`https://img.shields.io/github/v/release/eveseat/console.json`)
-const eveapi = getVersion(`https://img.shields.io/github/v/release/eveseat/eveapi.json`)
-const notifications = getVersion(`https://img.shields.io/github/v/release/eveseat/notifications.json`)
-const services = getVersion(`https://img.shields.io/github/v/release/eveseat/services.json`)
-const web = getVersion(`https://img.shields.io/github/v/release/eveseat/web.json`)
+const seatdocker = getVersion(`https://api.github.com/repos/eveseat/seat-docker/releases/latest`)
+const api = getVersion(`https://api.github.com/repos/eveseat/api/releases/latest`)
+const seat_console = getVersion(`https://api.github.com/repos/eveseat/console/releases/latest`)
+const eveapi = getVersion(`https://api.github.com/repos/eveseat/eveapi/releases/latest`)
+const notifications = getVersion(`https://api.github.com/repos/eveseat/notifications/releases/latest`)
+const services = getVersion(`https://api.github.com/repos/eveseat/services/releases/latest`)
+const web = getVersion(`https://api.github.com/repos/eveseat/web/releases/latest`)
 
-export const matcher = `.seat`
+export const matcher = `.version`
 export const handle = (m: Message): void => {
 	log.debug(`message from ${m.author.username}. sending a seat related response`);
 	Promise.all([seatdocker, api, seat_console, eveapi, notifications, services, web])
@@ -20,10 +20,36 @@ export const handle = (m: Message): void => {
 			.setColor('#0099ff')
 			.setTitle('SeAT Package Versions')
 			.addFields(
-		        { name:`\u200B`, value:
-		        `Seat Docker - [${values[0]}](https://hub.docker.com/r/eveseat/seat)\nAPI - [${values[1]}](https://github.com/eveseat/api/releases/latest)\nConsole (Depreciated) - [${values[2]}](https://github.com/eveseat/api/releases/latest)\nEveAPI - [${values[3]}](https://github.com/eveseat/api/releases/latest)\nNotifications - [${values[4]}](https://github.com/eveseat/api/releases/latest)\nServices - [${values[5]}](https://github.com/eveseat/api/releases/latest)\nWeb - [${values[6]}](https://github.com/eveseat/api/releases/latest)` },
+		        {
+		            name:`SeAT Docker`,
+		            value: `[${values[0].name}](${values[0].html_url})\nReleased - ${values[0].published_at}`
+		        },
+		        {
+		            name:`API`,
+		            value: `[${values[1].name}](${values[1].html_url})\nReleased - ${values[1].published_at}`
+		        },
+		        {
+		            name:`Console (Depreciated)`,
+		            value: `[${values[2].name}](${values[2].html_url})\nReleased - ${values[2].published_at}`
+		        },
+		        {
+		            name:`Eve API`,
+		            value: `[${values[3].name}](${values[3].html_url})\nReleased - ${values[3].published_at}`
+		        },
+		        {
+		            name:`Notifications`,
+		            value: `[${values[4].name}](${values[4].html_url})\nReleased - ${values[4].published_at}`
+		        },
+		        {
+		            name:`Services`,
+		            value: `[${values[5].name}](${values[5].html_url})\nReleased - ${values[5].published_at}`
+		        },
+		        {
+		            name:`Web`,
+		            value: `[${values[6].name}](${values[6].html_url})\nReleased - ${values[6].published_at}`
+		        }
 			);
-		    m.channel.send({ content: `${m.author}`, embeds: [mEmbed] });
+		    m.channel.send( { content: `${m.author}`, embeds: [mEmbed] });
 		})
 		.catch(error => {
             m.channel.send(`${m.author}, unable to determine current versions at this time`);
@@ -31,11 +57,11 @@ export const handle = (m: Message): void => {
 };
 
 function getVersion(version: string) {
-return axios.get(version)
-  .then(r => {
-    return (r.data.value)
-  })
-  .catch(error => {
-    log.debug(`Encountered error in .seat response`)
-  })
+    return axios.get(version)
+        .then(r => {
+            return (r.data)
+        })
+        .catch(error => {
+            log.debug('Encountered error in .version response')
+        })
 }
